7e1e6bb25ae5233cc36b10e36171907a62ef8b08
Adnan <ak567845@gmail.com>
2024-02-17 18:04:45 +0530

    updating

--- src/Components/CreateNote.jsx
+++ src/Components/CreateNote.jsx
@@ -27,13 +27,14 @@ const CreateNote = (props) => {
     <>
      <section className="main_div">
       <section className="main_note">
+      <Button onClick={addNotes} variant="contained"  sx={{":hover":{
+                bgcolor:"darkgoldenrod",color:"white"
+            },marginTop:"-20px",marginLeft:"240px",backgroundColor:"white",borderRadius:"50px"}}> <AddIcon sx={{color:"black"}}></AddIcon> </Button>
         <form >
             <input name = "title" onChange={getValue} value={note.title}  type="text" placeholder='Title' autoComplete='off'/><br/>
-            <textarea name="content" onChange={getValue} value={note.content} rows="" column="" placeholder='Write your note here...'></textarea>
+            <textarea name="content" onChange={getValue} value={note.content} rows="10" column="20" placeholder='Write your note here...'></textarea>
         </form>
-            <Button onClick={addNotes} variant="contained"  sx={{":hover":{
-                bgcolor:"darkgoldenrod",color:"white"
-            },marginTop:"155px",marginLeft:"240px",backgroundColor:"white",borderRadius:"50px"}}> <AddIcon sx={{color:"black"}}></AddIcon> </Button>
+           
       </section>
       </section>
     </>
